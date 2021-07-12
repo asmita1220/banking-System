4e933f9fe4ebe35449cb019442cd1c6de0581929
asmita1220 <85679627+asmita1220@users.noreply.github.com>
2021-07-12 12:54:45 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,26 @@
+<!-- navbar --> 
+      <nav class="navbar navbar-expand-md navbar-light bg-light">
+      <a class="navbar-brand" href="index.php" style="color : #495057; background-color: #f2f2f2;"><img src="img/images.jpg" alt="Logo" width="40" style="margin-bottom:4px;"> <b>BANKING SYSTEM</b></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color : #000000 ; font-size: 20px;"><b>Home</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="createuser.php" style="color : #000000 ;font-size: 20px;"><b>Create User</b></a>
+              </li>
+              
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php" style="color : #000000; font-size: 20px;"><b>Transfer Money</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color : #000000; font-size: 20px;"><b>Transaction History</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="removeuser.php" style="color : #000000; font-size: 20px;"><b>Remove User</b></a>
+              </li>
+          </div>
+       </nav>
